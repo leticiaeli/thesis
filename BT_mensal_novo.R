a2f9f0fd2d9bbbf0ea1410f184dfcdf5d10f7e11
Letícia Gabriela Eli <46682234+leticiaeli@users.noreply.github.com>
2019-10-22 13:14:12 -0300

    each month thermal balance

--- BT_mensal_novo.R
+++ BT_mensal_novo.R
@@ -126,6 +126,8 @@ df.balanco <- function(df, zt, zt1, internal_walls, external_walls, floor, roof,
 
 # FILES ----
 
+## DORMITÓRIO 1 ----
+
 # colnames(df)
 zt = 'DORM1'
 zt1 = 'DORMITORIO1' ## this output have a different zone name in my .idf
@@ -180,14 +182,64 @@ for(n in mes){
   }
 }
 
+# calculate the percentage for model using natural ventilation and ideal loads for each month
+
+library(plyr)
+# library(dplyr)
+days = count(df_vn$mes)/24 ## count the days for each month (each day have 24 hours)
+days$mes = c(1:12)
+VN = count(df_vn_ocupp$mes)/10 ## bedroom is occupied for 10 hours
+AC = count(df_ac_ocupp$mes)/10
+# 
+# df_month_vn = data.frame(mes=mes, dias=days$freq, porcentagem=VN$freq, group=c("VN"))
+# df_month_ac = data.frame(mes=mes, dias=days$freq, porcentagem=AC$freq, group=c("AC"))
+# 
+# df_month_vn$freq = round(((df_month_vn$porcentagem/df_month_vn$dias)*100),0) # divide days for VN / month days
+# df_month_ac$freq = round(((df_month_ac$porcentagem/df_month_ac$dias)*100),0)
+# 
+# df_month_vn$freq1 = paste(df_month_vn$freq, "%")
+# df_month_ac$freq1 = paste(df_month_ac$freq, "%")
+# 
+# df_month_ac$mes = c(0.75,1.75,2.75,3.75,4.75,5.75,6.75,7.75,8.75,9.75,10.75,11.75)
+# df_month_vn$mes = c(1.25,2.25,3.25,4.25,5.25,6.25,7.25,8.25,9.25,10.25,11.25,12.25)
+
+df_year = data.frame(VN=round(((sum(VN$freq)/365)*100),0),AC=round(((sum(AC$freq)/365)*100),0))
+
+df_year$VN = paste0(df_year$VN, "%")
+df_year$AC = paste0(df_year$AC, "%")
+
+# df_teste = data.frame(teste=c(1:132))
+# 
+# df_teste = data.frame(teste=df_month_vn$freq)
+# 
+# df_teste1 = data.frame(teste=1:120)
+# df_teste1$teste = NA
+# 
+# x = rbind(df_teste, df_teste1)
+# 
+# df_teste = data.frame(teste=df_month_ac$freq)
+# y = rbind(df_teste, df_teste1)
+# 
+# 
+# df_month = rbind(df_month_vn, df_month_ac)
+#  
+# df_month$freq = round((df_month$freq*100),0)
+
 ## plot
+
+setwd("D:/Frentes de Trabalho/simu_NBR/balanco_termico/resultados/mensal/nbr/")
 
 library(ggplot2)
 
 # create the groups (for the plot) in each df
 
 df_vn_graph$group = "VN"
+# df_vn_graph$freq = x$teste
+#df_vn_graph$freq = df_month_vn$freq
 df_ac_graph$group = "AC"
+# df_ac_graph$freq = y$teste
+#df_ac_graph$freq = df_month_ac$freq
+
 
 df_graph = rbind(df_vn_graph, df_ac_graph)
 
@@ -203,8 +255,8 @@ df_graph$mes.abb_pt = factor(df_graph$mes.abb_pt, levels=c("Jan","Fev","Mar","Ab
 
 # set the plot name and image
 
-graph_title = "Balanço Térmico no Ar do Dormitório 1 - Ventilação Natural (VN)"
-graph_png = "sp_d1_35_op_varia_mes_01.png"
+graph_title = "Balanço Térmico da Referência - Dormitório 1"
+graph_png = "SP_D1_Ref_r01.png"
 
 # set the graph limit
 
@@ -217,20 +269,289 @@ c1 = as.numeric(nearest(c))
 
 # the plot :)
 
-#png(filename = paste0(graph_png), width = 25, height = 15, units = "cm", res = 500)
-#plot(
+png(filename = paste0(graph_png), width = 25, height = 15, units = "cm", res = 500)
+plot(
   ggplot(df_graph, aes(x=mes.abb_pt, y=valor, fill=portugues, group=group)) +
     geom_bar(stat="identity", position=position_dodge(), color="gray")+
     labs(title=paste0(graph_title), y="Ganhos e Perdas no Ar da Zona [kWh]")+
+    #annotate("text", x = 1.5, y = -c1-1, label = "% DE DIAS NO MÊS COM AC ou VN:")+
+    # annotate("text", x = 1, y = -c1-5, label = "% dias no mês com AC:")+
+    #annotate("text", x = df_month_vn$mes, y = -c1-5, label = df_month_vn$freq1)+
+    #annotate("text", x = df_month_ac$mes, y = -c1-5, label = df_month_ac$freq1)+
+        #geom_text(aes(label=freq), vjust=-1.6, color="black",
+    #          position = position_dodge(0.9), size=3.5)+
+    annotate("text",x=4, y = c1, label = paste0("VN no ano: ",df_year$VN))+
+    annotate("text",x=9, y = c1, label = paste0("AC no ano: ",df_year$AC))+
     theme(legend.title = element_blank(),
           axis.text.x = element_text(size=12),
           axis.title.x = element_blank(),
           axis.title.y = element_text(size=12, vjust = 2),
           axis.text.y = element_text(size=10),
-          legend.text = element_text(size=10)
-          #strip.text.x = element_text(size=13)
-    )+
+          legend.text = element_text(size=10),
+          panel.background = element_rect(fill = "#F4F6F6",
+                                          colour = "#F4F6F6",
+                                          size = 0.5, linetype = "solid"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "#FEFEFE"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "#FEFEFE"))+
+    scale_fill_manual(values=c("#FFFF99","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#A6CEE3","#1F78B4", "#BC9EC1", "#E3BAC6"))+  # esse eh o Paired
+   ylim(-c1,c1)
+)
+dev.off()
+
+graph_png = "SP_D1_Ref_r02.png"
+
+group.labs = c()
+
+group.labs <- c(paste0("VN: ", df_year$VN, " dias no ano"), paste0("AC: ", df_year$AC, " dias no ano"))
+names(group.labs) <- c("VN", "AC")
+
+png(filename = paste0(graph_png), width = 25, height = 15, units = "cm", res = 500)
+plot(
+  ggplot(df_graph, aes(x=mes.abb_pt, y=valor, fill=portugues)) +
+    geom_bar(stat="identity", color="gray")+
+    labs(title=paste0(graph_title), y="Ganhos e Perdas no Ar da Zona [kWh]")+
+    #annotate("text", x = 1.5, y = -c1-1, label = "% DE DIAS NO MÊS COM AC ou VN:")+
+    # annotate("text", x = 1, y = -c1-5, label = "% dias no mês com AC:")+
+    #annotate("text", x = df_month_vn$mes, y = -c1-5, label = df_month_vn$freq1)+
+    #annotate("text", x = df_month_ac$mes, y = -c1-5, label = df_month_ac$freq1)+
+    #geom_text(aes(label=freq), vjust=-1.6, color="black",
+    #          position = position_dodge(0.9), size=3.5)+
+    #annotate("text",x=4, y = c1, label = paste0("VN no ano: ",df_year$VN))+
+    #annotate("text",x=9, y = c1, label = paste0("AC no ano: ",df_year$AC))+
+    theme(legend.title = element_blank(),
+          axis.text.x = element_text(size=12),
+          axis.title.x = element_blank(),
+          axis.title.y = element_text(size=12, vjust = 2),
+          axis.text.y = element_text(size=10),
+          legend.text = element_text(size=10),
+          panel.background = element_rect(fill = "#F4F6F6",
+                                          colour = "#F4F6F6",
+                                          size = 0.5, linetype = "solid"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "#FEFEFE"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "#FEFEFE"))+
+    scale_fill_manual(values=c("#FFFF99","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#A6CEE3","#1F78B4", "#BC9EC1", "#E3BAC6"))+  # esse eh o Paired
+    ylim(-c1,c1)+
+    facet_grid(group~.,
+               labeller = labeller(group = group.labs))
+)
+dev.off()
+
+#SALA ----
+
+#colnames(df)
+zt = 'SALA'
+zt1 = 'SALA1' ## this output have a different zone name in my .idf
+internal_walls = c('PARINT2_SALABWC','PARINT1_SALABWC','PARINT1_SALADORM2','PARINT2_SALADORM1')
+external_walls = c('PAREXT_SUL_SALA','PAREXT_OESTE_SALA','PAREXT_LESTE_SALA','PAREXT_NORTE_SALA')
+floor = c('PISO_SALA')
+roof = c('FORRO_SALAATICO1','FORRO_SALAATICO2')
+windows = c('JANSALA_OESTE','JANSALA_SUL')
+doors = c('PORTAEXT_NORTE','PORTAINT_SALABWC','PORTAINT_SALADORM1','PORTAINT_SALADORM2','PORTAEXT_LESTE')
+
+
+setwd("D:/Frentes de Trabalho/simu_NBR/balanco_termico/NBR/BRA_sp_Sao.Paulo-Congonhas.AP.837800_TMYx.2003-2017/")
+
+vn = read.csv("vn_Caso1_sp_sta_co_1.csv")
+
+df_vn = df.balanco(vn, zt, zt1, internal_walls, external_walls, floor, roof, windows, doors)
+df_vn$cooling = 0 # if I don't but these columns df_vn and df_ac will have a different lenght 
+df_vn$heating = 0
+
+setwd("D:/Frentes de Trabalho/simu_NBR/balanco_termico/NBR/HVAC/BRA_SP_Sao.Paulo-Congonhas.AP.837800_TMYx.2003-2017/")
+
+ac = read.csv("ac1_Caso1_sp_sta_co_1.csv")
+
+df_ac = df.balanco(ac, zt, zt1, internal_walls, external_walls, floor, roof, windows, doors)
+
+## thermal balance for the time when zone is occupied for which model (natural ventilation and ideal loads) 
+
+## calculate which time zone needs cooling or heating (the range is a and b)
+a = as.numeric(26)
+b = as.numeric(18)
+
+df_ac$temp_vn = df_vn$temp_op ## put the column for operative temperature from the model with natural ventilation
+
+df_vn_ocupp = subset(df_vn, (df_vn$people > 0) & (df_vn$temp_op < a) & (df_vn$temp_op > b)) ## subset df for occupied hours and with the range that we need use VN
+df_ac_ocupp = subset(df_ac, (df_ac$people > 0) & ((df_ac$temp_vn >= a) | (df_ac$temp_vn <= b))) ## subset df for occupied hours and with the range that we need use AC
+
+## create the df with gain and loss for which month
+
+ganho_perda = c("internal_gains","windows","internal_walls","external_walls","doors","floor","roof","vn_loss","vn_gain", "cooling", "heating")
+mes = c(1:12)
+
+df_vn_graph = data.frame(mes=mes, ganho_perda=rep(ganho_perda,each=12),valor = c(1:1))
+df_ac_graph = data.frame(mes=mes, ganho_perda=rep(ganho_perda,each=12),valor = c(1:1))
+
+for(n in mes){
+  for(k in ganho_perda){
+    for(i in 1:nrow(df_vn_graph)){
+      df_vn_graph$valor[df_vn_graph$mes == n & df_vn_graph$ganho_perda == k] = sum(df_vn_ocupp[,k][df_vn_ocupp$mes == n])
+    }
+    for(i in 1:nrow(df_ac_graph)){
+      df_ac_graph$valor[df_ac_graph$mes == n & df_ac_graph$ganho_perda == k] = sum(df_ac_ocupp[,k][df_ac_ocupp$mes == n])
+    }
+  }
+}
+
+# calculate the percentage for model using natural ventilation and ideal loads for each month
+
+library(plyr)
+# library(dplyr)
+days = count(df_vn$mes)/24 ## count the days for each month (each day have 24 hours)
+days$mes = c(1:12)
+VN = count(df_vn_ocupp$mes)/10 ## bedroom is occupied for 10 hours
+AC = count(df_ac_ocupp$mes)/10
+# 
+# df_month_vn = data.frame(mes=mes, dias=days$freq, porcentagem=VN$freq, group=c("VN"))
+# df_month_ac = data.frame(mes=mes, dias=days$freq, porcentagem=AC$freq, group=c("AC"))
+# 
+# df_month_vn$freq = round(((df_month_vn$porcentagem/df_month_vn$dias)*100),0) # divide days for VN / month days
+# df_month_ac$freq = round(((df_month_ac$porcentagem/df_month_ac$dias)*100),0)
+# 
+# df_month_vn$freq1 = paste(df_month_vn$freq, "%")
+# df_month_ac$freq1 = paste(df_month_ac$freq, "%")
+# 
+# df_month_ac$mes = c(0.75,1.75,2.75,3.75,4.75,5.75,6.75,7.75,8.75,9.75,10.75,11.75)
+# df_month_vn$mes = c(1.25,2.25,3.25,4.25,5.25,6.25,7.25,8.25,9.25,10.25,11.25,12.25)
+
+df_year = data.frame(VN=round(((sum(VN$freq)/365)*100),0),AC=round(((sum(AC$freq)/365)*100),0))
+
+df_year$VN = paste0(df_year$VN, "%")
+df_year$AC = paste0(df_year$AC, "%")
+
+# df_teste = data.frame(teste=c(1:132))
+# 
+# df_teste = data.frame(teste=df_month_vn$freq)
+# 
+# df_teste1 = data.frame(teste=1:120)
+# df_teste1$teste = NA
+# 
+# x = rbind(df_teste, df_teste1)
+# 
+# df_teste = data.frame(teste=df_month_ac$freq)
+# y = rbind(df_teste, df_teste1)
+# 
+# 
+# df_month = rbind(df_month_vn, df_month_ac)
+#  
+# df_month$freq = round((df_month$freq*100),0)
+
+## plot
+
+setwd("D:/Frentes de Trabalho/simu_NBR/balanco_termico/resultados/mensal/nbr/")
+
+library(ggplot2)
+
+# create the groups (for the plot) in each df
+
+df_vn_graph$group = "VN"
+# df_vn_graph$freq = x$teste
+#df_vn_graph$freq = df_month_vn$freq
+df_ac_graph$group = "AC"
+# df_ac_graph$freq = y$teste
+#df_ac_graph$freq = df_month_ac$freq
+
+
+df_graph = rbind(df_vn_graph, df_ac_graph)
+
+# put the names in portuguese
+
+df_graph$portugues = rep(c("Carga Interna","Janelas","Paredes Internas","Paredes Externas","Portas","Piso","Cobertura","Perdas por VN","Ganhos por VN","Refrigeração","Aquecimento"),each=12)
+df_graph$mes.abb_pt = c("Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez")
+
+# ggplot plot in alphabetic order so I need transform in factor the gains and losses names
+
+df_graph$portugues <- factor(df_graph$portugues, levels=c("Carga Interna","Paredes Internas","Paredes Externas","Piso","Cobertura","Portas","Janelas","Perdas por VN","Ganhos por VN","Refrigeração","Aquecimento"))
+df_graph$mes.abb_pt = factor(df_graph$mes.abb_pt, levels=c("Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"))
+
+# set the plot name and image
+
+graph_title = "Balanço Térmico da Referência - Sala"
+graph_png = "SP_SALA_Ref_r01.png"
+
+# set the graph limit
+
+max = max(df_graph$valor)
+min = min(df_graph$valor)
+
+c = as.numeric(ifelse(max > min*(-1), max, min*(-1)))
+
+c1 = as.numeric(nearest(c))
+
+# the plot :)
+
+png(filename = paste0(graph_png), width = 25, height = 15, units = "cm", res = 500)
+plot(
+  ggplot(df_graph, aes(x=mes.abb_pt, y=valor, fill=portugues, group=group)) +
+    geom_bar(stat="identity", position=position_dodge(), color="gray")+
+    labs(title=paste0(graph_title), y="Ganhos e Perdas no Ar da Zona [kWh]")+
+    #annotate("text", x = 1.5, y = -c1-1, label = "% DE DIAS NO MÊS COM AC ou VN:")+
+    # annotate("text", x = 1, y = -c1-5, label = "% dias no mês com AC:")+
+    #annotate("text", x = df_month_vn$mes, y = -c1-5, label = df_month_vn$freq1)+
+    #annotate("text", x = df_month_ac$mes, y = -c1-5, label = df_month_ac$freq1)+
+    #geom_text(aes(label=freq), vjust=-1.6, color="black",
+    #          position = position_dodge(0.9), size=3.5)+
+    annotate("text",x=4, y = c1, label = paste0("VN no ano: ",df_year$VN))+
+    annotate("text",x=9, y = c1, label = paste0("AC no ano: ",df_year$AC))+
+    theme(legend.title = element_blank(),
+          axis.text.x = element_text(size=12),
+          axis.title.x = element_blank(),
+          axis.title.y = element_text(size=12, vjust = 2),
+          axis.text.y = element_text(size=10),
+          legend.text = element_text(size=10),
+          panel.background = element_rect(fill = "#F4F6F6",
+                                          colour = "#F4F6F6",
+                                          size = 0.5, linetype = "solid"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "#FEFEFE"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "#FEFEFE"))+
     scale_fill_manual(values=c("#FFFF99","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#A6CEE3","#1F78B4", "#BC9EC1", "#E3BAC6"))+  # esse eh o Paired
     ylim(-c1,c1)
-#)
-#dev.off()
+)
+dev.off()
+
+graph_png = "SP_SALA_Ref_r02.png"
+
+group.labs = c()
+
+group.labs <- c(paste0("VN: ", df_year$VN, " dias no ano"), paste0("AC: ", df_year$AC, " dias no ano"))
+names(group.labs) <- c("VN", "AC")
+
+png(filename = paste0(graph_png), width = 25, height = 15, units = "cm", res = 500)
+plot(
+  ggplot(df_graph, aes(x=mes.abb_pt, y=valor, fill=portugues)) +
+    geom_bar(stat="identity", color="gray")+
+    labs(title=paste0(graph_title), y="Ganhos e Perdas no Ar da Zona [kWh]")+
+    #annotate("text", x = 1.5, y = -c1-1, label = "% DE DIAS NO MÊS COM AC ou VN:")+
+    # annotate("text", x = 1, y = -c1-5, label = "% dias no mês com AC:")+
+    #annotate("text", x = df_month_vn$mes, y = -c1-5, label = df_month_vn$freq1)+
+    #annotate("text", x = df_month_ac$mes, y = -c1-5, label = df_month_ac$freq1)+
+    #geom_text(aes(label=freq), vjust=-1.6, color="black",
+    #          position = position_dodge(0.9), size=3.5)+
+    #annotate("text",x=4, y = c1, label = paste0("VN no ano: ",df_year$VN))+
+    #annotate("text",x=9, y = c1, label = paste0("AC no ano: ",df_year$AC))+
+    theme(legend.title = element_blank(),
+          axis.text.x = element_text(size=12),
+          axis.title.x = element_blank(),
+          axis.title.y = element_text(size=12, vjust = 2),
+          axis.text.y = element_text(size=10),
+          legend.text = element_text(size=10),
+          panel.background = element_rect(fill = "#F4F6F6",
+                                          colour = "#F4F6F6",
+                                          size = 0.5, linetype = "solid"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "#FEFEFE"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "#FEFEFE"))+
+    scale_fill_manual(values=c("#FFFF99","#B2DF8A","#33A02C","#FB9A99","#E31A1C","#FDBF6F","#FF7F00","#A6CEE3","#1F78B4", "#BC9EC1", "#E3BAC6"))+  # esse eh o Paired
+    ylim(-c1,c1)+
+    facet_grid(group~.,
+               labeller = labeller(group = group.labs))
+)
+dev.off()
+
